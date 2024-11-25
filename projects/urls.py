262426f1232f1a75e701bd4e3640ef3c8b1c07a6
maritza1818 <maritzaclaudiamarronpuma@gmail.com>
2024-11-25 17:23:50 -0500

    Avanze Django CRUD con Autenticacion(Login,Register, Rutas protegidas, y mas)

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from rest_framework import routers
 from .api import ProjectViewSet
 
+from . import views
+
+urlpatterns = [
+    path('', views.helloworld, name='home'),  # Página principal dentro del proyecto
+    # Otras rutas específicas del proyecto
+]
+
 router = routers.DefaultRouter()
 router.register('api/projects', ProjectViewSet, 'projects')
 
